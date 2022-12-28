5b843372875978a6099e1a6193be73fc6730571d
Samir Romdhani <romdhanisamir94@gmail.com>
2022-12-28 21:11:54 +0100

    GIT:/ Version 0.2.4-rc.1

--- projects/scllib/src/public-api.ts
+++ projects/scllib/src/public-api.ts
@@ -5,9 +5,8 @@
 /*
  * Public API Surface of scl-lib
  */
-// export * from './lib/scllib.service';
+export * from './lib/scllib.service';
 export * from './lib/xmlns/SCL';
 // REST (Nest JS)
-// export * from './lib/scllib.serviceRest';
-import { ScllibServiceRest } from './lib/scllib.serviceRest';
+export * from './lib/scllib.serviceRest';
 export * from './lib/mappings/$Rest';
